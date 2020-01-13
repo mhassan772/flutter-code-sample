d83af981abef4fc0de67bd7b426f30559c07fcf8
Mohamed M. Hassan <mohamed.magdy.hassan@gmail.com>
2020-01-13 08:07:05 -0800

    Create statefull-skelton.dart

---
+++ statefull-skelton.dart
@@ -0,0 +1,30 @@
+// Import MaterialApp and other widgets which we can use to quickly create a material app
+import 'package:flutter/material.dart';
+
+void main() => runApp(new TodoApp());
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Todo List',
+        home: new TodoList()
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  createState() => new TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+            title: new Text('Todo List')
+        )
+    );
+  }
+}
