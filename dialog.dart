cad170c62af3f862a57655c05f8a1c91772a1b47
Mohamed M. Hassan <mohamed.magdy.hassan@gmail.com>
2020-01-14 07:55:43 -0800

    Update dialog.dart

--- dialog.dart
+++ dialog.dart
@@ -3,21 +3,21 @@ showDialog(
       builder: (BuildContext context) {
         // return object of type Dialog
         return AlertDialog(
-          title: new Text("Alert Dialog title"),
-          content: new Text("Alert Dialog body"),
+          title: new Text("Deleting Todo"),
+          content: new Text('Are you sure you want to delete "${task.text}" '),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
-                new FlatButton(
-            child: new Text('CANCEL'),
-            onPressed: () => Navigator.of(context).pop()
-          ),
-          new FlatButton(
-            child: new Text('MARK AS DONE'),
-            onPressed: () {
-              _removeTodoItem(index);
-              Navigator.of(context).pop();
-            }
-          )
+            new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () => Navigator.of(context).pop()
+            ),
+            new FlatButton(
+                child: new Text('DELETE'),
+                onPressed: () {
+                  _removeTodoItem(task);
+                  Navigator.of(context).pop();
+                }
+            ),
           ],
         );
       },
